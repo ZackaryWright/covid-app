16bd1fc43141372f77e3e5889cc8a9d15ec1f982
Zack Wright <zwrig004@fiu.edu>
2020-09-27 02:35:09 -0400

    Updates

---
+++ precache-manifest.46795780a28314f7d752633bd6555f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27303562b1d72e515be8bad356564a52",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "7e75f21652372b512de2",
+    "url": "/covid-app/static/css/main.8c8f917e.chunk.css"
+  },
+  {
+    "revision": "d48c9f4f880177c049dc",
+    "url": "/covid-app/static/js/2.3f795005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-app/static/js/2.3f795005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e75f21652372b512de2",
+    "url": "/covid-app/static/js/main.32507036.chunk.js"
+  },
+  {
+    "revision": "60a811fca23cbdf231cf",
+    "url": "/covid-app/static/js/runtime-main.61ce12e1.js"
+  }
+]);
